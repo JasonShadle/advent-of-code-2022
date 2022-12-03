6800e233d89940b5a2a97c40965e9ad5315f9cfb
Jason Shadle <jason.shadle50@gmail.com>
2022-12-03 00:21:38 +0000

    day 2 complete

--- day-02/part1.py
+++ day-02/part1.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+
+outcomeDict = {
+    'win': 6,
+    'tie': 3,
+    'lose': 0
+}
+symbolDict = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+}
+
+def getPoints(user, computer):
+    diff = symbolDict.get(user) - symbolDict.get(computer)
+    if (diff == 0):
+        return symbolDict.get(user) + outcomeDict.get("tie")
+    if (diff == 1 or diff == -2):
+        return symbolDict.get(user) + outcomeDict.get('win')
+   
+    return symbolDict.get(user) + outcomeDict.get('lose')
+
+total = 0
+for line in file:
+    inputs = line.strip().split(' ')
+    total = total + int(getPoints(inputs[1], inputs[0]))
+
+print(total)
--- day-02/part2.py
+++ day-02/part2.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+
+outcomeDict = {
+    'X': 0,
+    'Y': 3,
+    'Z': 6
+}
+symbolDict = {
+    'A': 1,
+    'B': 2,
+    'C': 3
+}
+pointChangeForOutcome = {
+    'X': -1,
+    'Y': 0,
+    'Z': 1
+}
+
+def getPoints(outcome, computer):
+    return getUserSymbolPointValue(outcome, computer) + outcomeDict.get(outcome)
+
+def getUserSymbolPointValue(outcome, computer):
+    pointChange = pointChangeForOutcome.get(outcome)
+    computerPoints = symbolDict.get(computer)
+    return list(symbolDict.values())[(computerPoints + pointChange - 1) % 3]
+
+total = 0
+for line in file:
+    inputs = line.strip().split(' ')
+    total = total + getPoints(inputs[1], inputs[0])
+
+print(total)
