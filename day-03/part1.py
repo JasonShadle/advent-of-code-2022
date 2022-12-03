9c72e589a116a12e89033753c25b2677b071be22
Jason Shadle <jason.shadle50@gmail.com>
2022-12-03 20:04:17 +0000

    day 3 complete

--- day-03/part1.py
+++ day-03/part1.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+commonLetters = []
+
+for line in file:
+    line = line.strip()
+    sets = [line[:len(line)//2],line[len(line)//2:]]
+    commonLetters.append((set(sets[0]) & set(sets[1])).pop()[0])
+
+total = 0
+print(commonLetters)
+for letter in commonLetters:
+    offset = 38 if letter.isupper() else 96
+    total = total + ord(letter) - offset
+
+print(total)
--- day-03/part2.py
+++ day-03/part2.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+lines = file.read().splitlines()
+
+commonLetters = []
+
+for i in range(0, len(lines) // 3):
+    j = i*3
+    commonLetters.append(set(lines[j]).intersection(lines[j+1]).intersection(lines[j+2]).pop()[0])
+
+total = 0
+
+for letter in commonLetters:
+    offset = 38 if letter.isupper() else 96
+    total = total + ord(letter) - offset
+
+print(total)
