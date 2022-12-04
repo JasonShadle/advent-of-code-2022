74347b42477c17680d5147f6c3c201e12e9d9c75
Jason Shadle <jason.shadle50@gmail.com>
2022-12-04 15:03:32 +0000

    day 4 complete

--- day-04/part2.py
+++ day-04/part2.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+totalOverlaps = 0
+def contains_full_range(elf1, elf2):
+    elf1_range = [int(x) for x in elf1.split('-')]
+    elf2_range = [int(x) for x in elf2.split('-')]
+
+    if (elf1_range[0] <= elf2_range[0] and elf1_range[1] >= elf2_range[0]
+        or (elf2_range[0] <= elf1_range[0] and elf2_range[1] >= elf1_range[0])):
+        return True
+    return False
+
+for line in file:
+    line = line.strip()
+    assignments = line.split(',')
+    if (contains_full_range(assignments[0], assignments[1])): totalOverlaps = totalOverlaps + 1
+
+print(totalOverlaps)
