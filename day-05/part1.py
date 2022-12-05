a3fc6ffc10c515a148c94617b43bf2fb35972cec
Jason Shadle <jason.shadle50@gmail.com>
2022-12-05 23:36:55 +0000

    day 5 complete

--- day-05/part1.py
+++ day-05/part1.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+start_grid = [
+    ['S','L','F','Z','D','B','R','H'],
+    ['R','Z','M','B','T'],
+    ['S','N','H','C','L','Z'],
+    ['J','F','C','S'],
+    ['B','Z','R','W','H','G','P'],
+    ['T','M','N','D','G','Z','J','V'],
+    ['Q','P','S','F','W','N','L','G'],
+    ['R','Z','M'],
+    ['T','R','V','G','L','C','M']
+]
+has_started = False
+
+for line in file:
+    line = line.strip()
+    input_split = line.split()
+    amount = int(input_split[1])
+    from_index = int(input_split[3]) - 1
+    to_index = int(input_split[5]) - 1
+
+    for x in range(0,amount):
+        letter = start_grid[from_index].pop(0)
+        start_grid[to_index].insert(0, letter)
+
+topOrder = ''
+for line in start_grid:
+    topOrder = topOrder + line.pop(0)
+
+print(topOrder)
--- day-05/part2.py
+++ day-05/part2.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+
+start_grid = [
+    ['S','L','F','Z','D','B','R','H'],
+    ['R','Z','M','B','T'],
+    ['S','N','H','C','L','Z'],
+    ['J','F','C','S'],
+    ['B','Z','R','W','H','G','P'],
+    ['T','M','N','D','G','Z','J','V'],
+    ['Q','P','S','F','W','N','L','G'],
+    ['R','Z','M'],
+    ['T','R','V','G','L','C','M']
+]
+has_started = False
+
+for line in file:
+    line = line.strip()
+    input_split = line.split()
+    amount = int(input_split[1])
+    from_index = int(input_split[3]) - 1
+    to_index = int(input_split[5]) - 1
+
+    crates_to_add = []
+    for x in range(amount-1, -1, -1):
+        letter = start_grid[from_index].pop(x)
+        start_grid[to_index].insert(0, letter)
+
+topOrder = ''
+for line in start_grid:
+    topOrder = topOrder + line.pop(0)
+
+print(topOrder)
