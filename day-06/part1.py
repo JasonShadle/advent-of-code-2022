564c0a829db8103b0046337a04234e05dfe185be
Jason Shadle <jason.shadle50@gmail.com>
2022-12-06 23:05:13 +0000

    day 6 complete

--- day-06/part1.py
+++ day-06/part1.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+segment_length = 4
+
+for line in file:
+    line = line.strip()
+    
+    for startIndex in range(0, len(line) - segment_length - 1):
+        segment = line[startIndex: startIndex + segment_length]
+        # print(segment, set(segment))
+        if (len(set(segment)) == len(segment)): 
+            print(startIndex + segment_length)
+            break
--- day-06/part2.py
+++ day-06/part2.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+segment_length = 14
+
+for line in file:
+    line = line.strip()
+    
+    for startIndex in range(0, len(line) - segment_length - 1):
+        segment = line[startIndex: startIndex + segment_length]
+        if (len(set(segment)) == len(segment)): 
+            print(startIndex + segment_length)
+            break
