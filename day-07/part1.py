449dc7d7db930296c1ae4f0aad03b29cb871fb3a
Jason Shadle <jason.shadle50@gmail.com>
2022-12-09 02:13:41 +0000

    day 7 done (yes im late)

--- day-07/part1.py
+++ day-07/part1.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
+cwd = ''
+dir_sizes = {}
+
+def addToAllDirs(size, path):
+    # if (path == '/'):
+    dir_sizes[path] = dir_sizes.get(path, 0) + int(size)
+
+    while (len(path) > 1):
+        path = path[:path.rfind('/')]
+        if len(path) == 0: path = '/'
+        dir_sizes[path] = dir_sizes.get(path, 0) + int(size)
+
 file = open('input.txt', 'r')
+
+for line in file:
+    line = line.strip().split(' ')
+    
+    if line[0] == '$' and line[1] == 'cd' and line[2] == '..':
+        cwd = cwd[:cwd.rfind('/')]
+
+    elif line[0] == '$' and line[1] == 'cd':
+        if line[2] == '/':
+            cwd = '/'
+        elif cwd.endswith('/'): 
+            cwd = cwd + line[2]
+        else:
+            cwd = cwd + '/' + line[2]
+
+    elif line[0].isnumeric():
+        addToAllDirs(line[0], cwd)
+
+total = 0
+for key in dir_sizes:
+    if dir_sizes.get(key) <= 100000:
+        total = total + dir_sizes[key]
+
+print(total)
--- day-07/part2.py
+++ day-07/part2.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python3
 file = open('input.txt', 'r')
+cwd = ''
+dir_sizes = {}
+
+def addToAllDirs(size, path):
+    # if (path == '/'):
+    dir_sizes[path] = dir_sizes.get(path, 0) + int(size)
+
+    while (len(path) > 1):
+        path = path[:path.rfind('/')]
+        if len(path) == 0: path = '/'
+        dir_sizes[path] = dir_sizes.get(path, 0) + int(size)
+
+file = open('input.txt', 'r')
+for line in file:
+    line = line.strip().split(' ')
+    
+    if line[0] == '$' and line[1] == 'cd' and line[2] == '..':
+        cwd = cwd[:cwd.rfind('/')]
+    elif line[0] == '$' and line[1] == 'cd':
+        if line[2] == '/':
+            cwd = '/'
+        elif cwd.endswith('/'): 
+            cwd = cwd + line[2]
+        else:
+            cwd = cwd + '/' + line[2]
+    elif line[0].isnumeric():
+        addToAllDirs(line[0], cwd)
+
+current_closest_size = 9999999999999
+for d in dir_sizes:
+    difference = 70000000 - dir_sizes.get('/') + dir_sizes.get(d)
+    if difference >= 30000000 and dir_sizes.get(d) < current_closest_size:
+        current_closest_size = dir_sizes.get(d)
+
+print(current_closest_size)
